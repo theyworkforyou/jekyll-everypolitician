82637f7398b1bd1c40b9521b23981411de4651eb
Chris Mytton <chrism@mysociety.org>
2015-12-03 18:13:10 +0000

    Remove activesupport dependency
    Jekyll comes with support for creating slugs from strings, so use that
    rather than depending on activesupport.

--- lib/jekyll/everypolitician.rb
+++ lib/jekyll/everypolitician.rb
@@ -4,8 +4,6 @@ require 'json'
 require 'open-uri'
 
 require 'jekyll'
-require 'active_support'
-require 'active_support/core_ext/string'
 
 module Jekyll
   module Everypolitician
@@ -37,7 +35,7 @@ module Jekyll
           collection = Collection.new(site, collection_name)
           popolo[type].each do |item|
             next unless item['id']
-            path = File.join(site.source, "_#{collection_name}", "#{item['id'].parameterize}.md")
+            path = File.join(site.source, "_#{collection_name}", "#{Jekyll::Utils.slugify(item['id'])}.md")
             doc = Document.new(path, collection: collection, site: site)
             doc.merge_data!(item)
             doc.merge_data!(
